2a12f0e369f846bda7fd1f034539f0c2f894afa8
Dennis van Dok <dennisvd@nikhef.nl>
2012-01-21 12:52:26 +0100

    added download script for original

---
+++ debian/orig-tar.sh
@@ -0,0 +1,12 @@
+#!/bin/sh -e
+
+VERSION=$2
+TAR=../argus-pdp-pep-common_$VERSION.orig.tar.gz
+DIR=argus-pdp-pep-common-$VERSION
+TAG=$VERSION
+REPO=http://svn.cern.ch/guest/glxa/pdp-pep-java-common
+
+svn export $REPO/tags/${TAG} $DIR
+GZIP=--best tar -c -z -f $TAR --exclude '*.jar' --exclude '*.class' $DIR
+rm -rf $DIR 
+rm ../$TAG
